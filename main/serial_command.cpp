2b3fb2fe62c6965858b02dbbfa0fb8333077080f
PenguinDevs <34122116+PenguinDevs@users.noreply.github.com>
2022-09-15 19:18:25 +1000

    proper debugging info

--- main/serial_command.cpp
+++ main/serial_command.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "serial_command.hpp"
 #include "motor.hpp"
+#include "debugger.hpp"
 
 using namespace firmware::command;
 
@@ -10,10 +11,11 @@ void firmware::command::pollSerial() {
   if (Serial.available() > 0) {
     String input = Serial.readString();
     input.trim();
-    Serial.println("RAW" + input);
 
     String command = input.substring(0, 3);
     String param = input.substring(4);
+
+    firmware::debugger::info("SERIAL RECEIVED: " + command + param);
 
     // Unfortunately I cannot use a string to compare in a switch case. :/
     // TODO: Figure out a way for ^, maybe somehow retrieve unicode from
@@ -21,8 +23,9 @@ void firmware::command::pollSerial() {
     if (command == "dir") {  // Steer direction
         float direction = param.toFloat();
         motorPair.setSteer(direction);
-    }
 
-    Serial.println(command + param);
+        firmware::debugger::info(
+            "Motor pair direction set to " + String(direction));
+    }
   }
 }
